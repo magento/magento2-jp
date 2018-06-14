a62c56ec9312590b48c1dc368c4bfdf0d7aa1489
Hirokazu Nishi <nishi@principle-works.jp>
2018-06-14 19:01:35 +0900

    remove unused class. delete unused method. update di.xml

--- Price/Model/Directory/Plugin/Precision.php
+++ Price/Model/Directory/Plugin/Precision.php
@@ -24,42 +24,6 @@ class Precision
     }
 
     /**
-     * Modify price precision for JPY
-     *
-     * @param Currency $subject          Currency Object
-     * @param \Closure $proceed          Closure
-     * @param float    $price            Price
-     * @param int      $precision        Currency Precision
-     * @param array    $options          Currency options array
-     * @param bool     $includeContainer Include container flag
-     * @param bool     $addBrackets      Add brackets flag
-     *
-     * @return mixed
-     */
-    public function aroundFormatPrecision(
-        Currency  $subject,
-        \Closure $proceed,
-        $price,
-        $precision = 2,
-        $options = [],
-        $includeContainer = true,
-        $addBrackets = false
-    ) {
-        if (in_array($subject->getCode(), $this->helper->getIntegerCurrencies())) {
-            $precision = '0';
-            if (isset($options['precision'])) {
-                $options['precision'] = '0';
-            }
-        }
-        return $proceed(
-            $price,
-            $precision,
-            $options,
-            $includeContainer,
-            $addBrackets);
-    }
-
-    /**
      * Modify Currency Position
      *
      * @param Currency $subject
