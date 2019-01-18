0a347c82f8196a256d52e6e2e85ed3c53dc82fad
Ievgen Shakhsuvarov <ishakhsuvarov@magento.com>
2019-01-18 09:38:52 -0600

    Zip2address feature coding practices improvement

---
+++ Zip2address/view/frontend/web/js/action/zip2address.js
@@ -0,0 +1,35 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    return {
+        apiEndpoint: 'https://madefor.github.io/postal-code-api/api/v1',
+        noDashPostcodeMask: new RegExp(/^[0-9]{7}$/),
+        hasDashPostcodeMask: new RegExp(/^[0-9]{3}\-?[0-9]{4}$/),
+
+        /**
+         * Convert JP postcode into JP address using 3rd-party service.
+         *
+         * @param {String} postcode
+         * @return {*}
+         */
+        convert: function (postcode) {
+            var firstPart, secondPart;
+
+            if (!(this.noDashPostcodeMask.test(postcode) || this.hasDashPostcodeMask.test(postcode))) {
+                return;
+            }
+
+            firstPart = postcode.replace(/^([0-9]{3}).*/, '$1');
+            secondPart = postcode.replace(/.*([0-9]{4})$/, '$1');
+
+            return $.getJSON(
+                this.apiEndpoint + '/' + firstPart + '/' + secondPart + '.json',
+                {
+                    cache: false
+                }
+            );
+        }
+    }
+});
--- Zip2address/view/frontend/web/js/zip2address.js
+++ Zip2address/view/frontend/web/js/zip2address.js
@@ -1,8 +1,10 @@
 define([
     'jquery',
+    'underscore',
     'mage/translate',
+    'MagentoJapan_Zip2address/js/action/zip2address',
     'Magento_Ui/js/modal/alert'
-], function ($, $t, alert) {
+], function ($, _, $t, Zip2address, alert) {
     'use strict';
 
     $.widget('vw.zip2address', {
@@ -18,35 +20,25 @@ define([
          */
         _search: function (event) {
             var postcode = event.target.value,
-                noDash = new RegExp(/^[0-9]{7}$/),
-                hasDash = new RegExp(/^[0-9]{3}\-?[0-9]{4}$/),
-                endpoint = 'https://madefor.github.io/postal-code-api/api/v1',
                 region = this.options.region,
                 city = this.options.city,
                 street = this.options.street,
                 lang = this.options.lang === 'ja_JP' ? 'ja' : 'en',
-                code1,
-                code2;
+                convert = Zip2address.convert(postcode);
 
-            if (noDash.test(postcode) || hasDash.test(postcode)) {
-                code1 = postcode.replace(/^([0-9]{3}).*/, '$1');
-                code2 = postcode.replace(/.*([0-9]{4})$/, '$1');
-
-                $.getJSON(
-                    endpoint + '/' + code1 + '/' + code2 + '.json',
-                    {
-                        cache: false
-                    }
-                ).done(function (data) {
-                    $(region)[0][data.data[0].prefcode].selected = true;
-                    $(city).val(data.data[0][lang].address1);
-                    $(street).val(data.data[0][lang].address2);
-                }).fail(function () {
-                    alert({
-                        content: $t('Provided Zip/Postal Code seems to be invalid.')
-                    });
-                });
+            if (_.isUndefined(convert)) {
+                return;
             }
+
+            convert.success(function (data) {
+                $(region)[0][data.data[0].prefcode].selected = true;
+                $(city).val(data.data[0][lang].address1);
+                $(street).val(data.data[0][lang].address2);
+            }).fail(function () {
+                alert({
+                    content: $t('Provided Zip/Postal Code seems to be invalid.')
+                });
+            });
         }
     });
 
