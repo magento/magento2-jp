9c98dbe9787cd1e9022aaa88692c0d9f97e2886d
Volodymyr Kublytskyi <vkublytskyi@magento.com>
2019-03-12 11:46:35 -0500

    Fixed language pack name issue which broke dictionary loading.

--- _phrases/registration.php
+++ _phrases/registration.php
@@ -7,4 +7,4 @@ declare(strict_types=1);
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-//ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'Magento_ja_jp', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'magento_ja_jp', __DIR__);
