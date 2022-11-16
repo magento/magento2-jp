6e5c4620b5ad77b338c0fbeddd4cd370f2696ce8
Nguyễn Thiện Hùng <nthung@ninepoints.vn>
2022-11-16 10:01:18 +0700

    - Fixed:     + Error round for number has decimal (5e15).     + Error price-utils-mixins.js (keep the algo by Magento)

--- CurrencyPrecision/Model/CurrencyRounding.php
+++ CurrencyPrecision/Model/CurrencyRounding.php
@@ -9,6 +9,15 @@ namespace CommunityEngineering\CurrencyPrecision\Model;
 
 use CommunityEngineering\CurrencyPrecision\Model\Config\CurrencyRoundingConfig;
 use CommunityEngineering\CurrencyPrecision\Model\Config\Source\RoundingMode;
+use Magento\Directory\Model\Currency;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\ScopeResolverInterface;
+use Magento\Framework\Locale\ResolverInterface as LocalResolverInterface;
+use Magento\Framework\NumberFormatterFactory;
+use Magento\Framework\Serialize\Serializer\Json;
+
+use function hash;
+use function sprintf;
 
 /**
  * Currency rounding service.
@@ -16,16 +25,58 @@ use CommunityEngineering\CurrencyPrecision\Model\Config\Source\RoundingMode;
 class CurrencyRounding
 {
     /**
-     * @var CurrencyRoundingConfig
+     * @var \Magento\Framework\App\ScopeResolverInterface
      */
-    private $currencyRoundingConfig;
+    protected $scopeResolver;
 
     /**
-     * @param CurrencyRoundingConfig $currencyRoundingConfig
+     * @var LocalResolverInterface
      */
-    public function __construct(CurrencyRoundingConfig $currencyRoundingConfig)
-    {
+    protected $localeResolver;
+
+    /**
+     * @var CurrencyRoundingConfig
+     */
+    protected $currencyRoundingConfig;
+
+    /**
+     * @var \Magento\Framework\NumberFormatterFactory
+     */
+    protected $numberFormatterFactory;
+
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json
+     */
+    protected $serializer;
+
+    /**
+     * @var \Magento\Framework\NumberFormatter|null
+     */
+    protected $_numberFormatter;
+
+    protected $_numberFormatterCache = [];
+
+    /**
+     * CurrencyRounding constructor.
+     *
+     * @param \Magento\Framework\Locale\ResolverInterface $localeResolver
+     * @param \CommunityEngineering\CurrencyPrecision\Model\Config\CurrencyRoundingConfig $currencyRoundingConfig
+     * @param \Magento\Framework\NumberFormatterFactory|null $numberFormatterFactory
+     * @param \Magento\Framework\Serialize\Serializer\Json|null $serializer
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        ScopeResolverInterface $scopeResolver,
+        LocalResolverInterface $localeResolver,
+        CurrencyRoundingConfig $currencyRoundingConfig,
+        NumberFormatterFactory $numberFormatterFactory = null,
+        Json $serializer = null
+    ) {
+        $this->scopeResolver = $scopeResolver;
+        $this->localeResolver = $localeResolver;
         $this->currencyRoundingConfig = $currencyRoundingConfig;
+        $this->numberFormatterFactory = $numberFormatterFactory ?: ObjectManager::getInstance()->get(NumberFormatterFactory::class);
+        $this->serializer = $serializer ?: ObjectManager::getInstance()->get(Json::class);
     }
 
     /**
@@ -35,12 +86,15 @@ class CurrencyRounding
      * so USD has 2 significant fraction digits. Japanese Yen (JPY) has precision 0 as no subunits currently in use.
      *
      * @param string $currencyCode
+     *
      * @return int
      */
     public function getPrecision(string $currencyCode): int
     {
         $formatter = $this->createCurrencyFormatter($currencyCode);
+
         $precision = $formatter->getAttribute(\NumberFormatter::MAX_FRACTION_DIGITS);
+
         return $precision;
     }
 
@@ -51,17 +105,31 @@ class CurrencyRounding
      *
      * @param string $currencyCode
      * @param float $amount
+     *
      * @return float
      */
     public function round(string $currencyCode, float $amount): float
     {
         $roundingMode = $this->getRoundingMode();
 
-        $formatter = $this->createCurrencyFormatter($currencyCode);
-        $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, $roundingMode);
+        /**
+         * Fix problem with 12 000 000, 1 200 000
+         *
+         * %f - the argument is treated as a float, and presented as a floating-point number (locale aware).
+         * %F - the argument is treated as a float, and presented as a floating-point number (non-locale aware).
+         */
+        $amount = (float)sprintf("%F", $amount);
 
-        $formatted = $formatter->format($amount);
+        $formatter = $this->createCurrencyFormatter($currencyCode);
+
+        $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, $roundingMode);
+        $formatted = $formatter->formatCurrency(
+            $amount,
+            $formatter->getTextAttribute(\NumberFormatter::CURRENCY_CODE)
+        );
+
         $rounded = $formatter->parse($formatted, \NumberFormatter::TYPE_DOUBLE);
+
         return $rounded;
     }
 
@@ -69,11 +137,51 @@ class CurrencyRounding
      * Create Intl Number Formatter for currency.
      *
      * @param string $currencyCode
-     * @return \NumberFormatter
+     * @param array $options
+     * @param string|null $localeCode
+     * @param string|int|null $style
+     *
+     * @return \Magento\Framework\NumberFormatter
      */
-    private function createCurrencyFormatter(string $currencyCode): \NumberFormatter
+    public function createCurrencyFormatter(string $currencyCode, array $options = [], $localeCode = null, $style = null): \Magento\Framework\NumberFormatter
     {
-        return new \NumberFormatter('@currency=' . $currencyCode, \NumberFormatter::CURRENCY);
+        $this->_numberFormatter = $this->getNumberFormatter($currencyCode, $options);
+
+        return $this->_numberFormatter;
+    }
+
+    /**
+     * @param string|null $currencyCode
+     * @param array $options
+     * @param string|null $localeCode
+     * @param string|int|null $style
+     *
+     * @return \Magento\Framework\NumberFormatter
+     */
+    public function getNumberFormatter($currencyCode = null, array $options = [], $localeCode = null, $style = null): \Magento\Framework\NumberFormatter
+    {
+        /** @var Currency $currency */
+        if (!$currencyCode) {
+            $currency = $this->scopeResolver->getScope()->getCurrentCurrency();
+            $currencyCode = $currency->getCode();
+        }
+
+        $localeCode = $localeCode ?: $this->localeResolver->getLocale();
+        $key = 'currency_' . hash(
+                'sha256',
+                ($localeCode . $this->serializer->serialize($options) . $currencyCode)
+            );
+
+        if (!isset($this->numberFormatterCache[$key])) {
+            $this->_numberFormatter = $this->numberFormatterFactory->create([
+                'locale' => $currencyCode ? $localeCode . '@currency=' . $currencyCode : $localeCode,
+                'style' => $style ?: \NumberFormatter::CURRENCY,
+            ]);
+
+            $this->_numberFormatterCache[$key] = $this->_numberFormatter;
+        }
+
+        return $this->_numberFormatterCache[$key];
     }
 
     /**
@@ -83,7 +191,7 @@ class CurrencyRounding
      *
      * @return int
      */
-    private function getRoundingMode(): int
+    protected function getRoundingMode(): int
     {
         $roundingModesMap = [
             RoundingMode::UP => \NumberFormatter::ROUND_UP,
@@ -94,7 +202,7 @@ class CurrencyRounding
             RoundingMode::HALFEVEN => \NumberFormatter::ROUND_HALFEVEN,
             RoundingMode::HALFDOWN => \NumberFormatter::ROUND_HALFDOWN,
         ];
-        $configuredMode =  $this->currencyRoundingConfig->getRoundingMode();
+        $configuredMode = $this->currencyRoundingConfig->getRoundingMode();
 
         if (!isset($roundingModesMap[$configuredMode])) {
             throw new \LogicException(
---
+++ CurrencyPrecision/view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Catalog/js/price-utils': {
+                'CommunityEngineering_CurrencyPrecision/js/price-utils-mixins': true
+            },
+            priceUtils: {
+                'CommunityEngineering_CurrencyPrecision/js/price-utils-mixins': true
+            }
+        }
+    }
+};
